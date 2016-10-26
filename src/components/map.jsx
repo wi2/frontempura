fc92b5df793f6c19eebe10d26181fb896d14461b
Thomas Haddad <thomas.haddad@af83.com>
2016-10-26 18:33:28 +0200

    Add openlayer and store
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

---
+++ src/components/map.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ol from 'openlayers'
+
+
+const OlMap = () => {
+  return <div id="map" className="map">CARTE</div>
+}
+
+export default OlMap
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import {render} from 'react-dom'
+import OlMap from './components/map'
+import ol from 'openlayers'
 
 import styles from './styles/main.sass'
 
+
+let store = createStore(reducer, map)
+
 render(
-  <div>Hello world</div>,
+  <Provider store={store}>
+    <OlMap/>
+  </Provider>,
   document.getElementById("root")
 )
+
+function reducer(state = [], action){
+  return state
+}
+
+let map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: [949282, 6002552],
+    zoom: 4
+  })
+});
+
+console.log(store.getState());
