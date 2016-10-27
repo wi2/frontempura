9f27e99dd57474f772c0fc31d8f7c2d7ef3e06e3
Thomas Haddad <thomas.haddad@af83.com>
2016-10-27 10:39:30 +0200

    Remove makefile for the moment, run yarn commands separately
    Signed-off-by: Thomas Shawarma Haddad <thomas.haddad@af83.com>

--- Makefile
+++
@@ -1,2 +0,0 @@
-run:
-	yarn run webpack | yarn run webpack-dev-server
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import ol from 'openlayers'
 
 
 const OlMap = () => {
-  return <div id="map" className="map">CARTE</div>
+  return <div id="map" className="map"></div>
 }
 
 export default OlMap
